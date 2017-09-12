613157aa6b1963cdfac724b7ea6eb79ec4928924
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-09-11 18:11:45 -0700

    change slate to be a monorepo using lerna (#1106)
    * init lerna, move files into ./packages
    
    * move test files into ./packages
    
    * more moving around
    
    * fill out package.json files
    
    * fixing imports
    
    * more fixing of imports, and horribleness
    
    * convert examples, fix linting errors
    
    * add documentation
    
    * update docs
    
    * get tests passing
    
    * update travis.yml
    
    * update travis.yml
    
    * update travis.yml
    
    * update test script
    
    * update travis.yml
    
    * update scripts
    
    * try simplifying travis.yml
    
    * ocd stuff
    
    * remove slate-core-test-helpers package
    
    * add package readmes
    
    * update reference docs structure
    
    * refactor slate-simulator into its own package
    
    * add docs for new packages
    
    * update docs
    
    * separate benchmarks into packages, and refactor them

---
+++ src/constants/transfer-types.js
@@ -0,0 +1,18 @@
+/**
+ * Slate-specific data transfer types.
+ *
+ * @type {Object}
+ */
+
+const TYPES = {
+  FRAGMENT: 'application/x-slate-fragment',
+  NODE: 'application/x-slate-node',
+}
+
+/**
+ * Export.
+ *
+ * @type {Object}
+ */
+
+export default TYPES
---
+++ src/utils/set-transfer-data.js
@@ -0,0 +1,47 @@
+
+/**
+ * Set data with `type` and `content` on a `dataTransfer` object.
+ *
+ * COMPAT: In Edge, custom types throw errors, so embed all non-standard
+ * types in text/plain compound object. (2017/7/12)
+ *
+ * @param {DataTransfer} dataTransfer
+ * @param {String} type
+ * @param {String} content
+ */
+
+function setTransferData(dataTransfer, type, content) {
+  try {
+    dataTransfer.setData(type, content)
+  } catch (err) {
+    const prefix = 'SLATE-DATA-EMBED::'
+    const text = dataTransfer.getData('text/plain')
+    let obj = {}
+
+    // If the existing plain text data is prefixed, it's Slate JSON data.
+    if (text.substring(0, prefix.length) === prefix) {
+      try {
+        obj = JSON.parse(text.substring(prefix.length))
+      } catch (e) {
+        throw new Error('Failed to parse Slate data from `DataTransfer` object.')
+      }
+    }
+
+    // Otherwise, it's just set it as is.
+    else {
+      obj['text/plain'] = text
+    }
+
+    obj[type] = content
+    const string = `${prefix}${JSON.stringify(obj)}`
+    dataTransfer.setData('text/plain', string)
+  }
+}
+
+/**
+ * Export.
+ *
+ * @type {Function}
+ */
+
+export default setTransferData
